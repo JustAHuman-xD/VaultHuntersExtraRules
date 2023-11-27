6118ab4205a7d52994341d9522e739e5bf3391c4
BONNe <bonne@bonne.id.lv>
2023-11-27 20:30:05 +0200

    Fixes copiously drop fix.

--- src/main/java/lv/id/bonne/vaulthuntersextrarules/mixin/MixinVaultBlockOre.java
+++ src/main/java/lv/id/bonne/vaulthuntersextrarules/mixin/MixinVaultBlockOre.java
@@ -9,16 +9,13 @@ package lv.id.bonne.vaulthuntersextrarules.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import iskallia.vault.block.VaultOreBlock;
 import lv.id.bonne.vaulthuntersextrarules.VaultHuntersExtraRules;
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.storage.loot.LootContext;
@@ -30,38 +27,33 @@ import net.minecraft.world.level.storage.loot.LootContext;
 @Mixin(VaultOreBlock.class)
 public class MixinVaultBlockOre
 {
-    @Inject(method = "getDrops",
-        at = @At(value = "INVOKE_ASSIGN", target = "Ljava/util/List;addAll(Ljava/util/Collection;)Z"),
-        locals = LocalCapture.CAPTURE_FAILSOFT,
-        remap = false)
-    private void increaseLootQuantity(BlockState state,
-        LootContext.Builder builder,
-        CallbackInfoReturnable<List<ItemStack>> cir,
-        List<ItemStack> drops,
-        ItemStack stack,
-        float chance,
-        Entity player,
-        BlockPos pos)
+    /**
+     * This method redirects to increaseLootQuantity.
+     * @param originalInstance The originalInstance.
+     * @param dropCopy The collection.
+     * @param state The block state.
+     * @param builder The loot context builder.
+     */
+    @Redirect(method = "getDrops",
+        at = @At(value = "INVOKE", target = "Ljava/util/List;addAll(Ljava/util/Collection;)Z"))
+    private boolean increaseCopiouslyDrop(List<ItemStack> originalInstance,
+        Collection<ItemStack> dropCopy,
+        BlockState state,
+        LootContext.Builder builder)
     {
-        int modifier = player.getLevel().getGameRules().
+        // Get modifier value
+        int modifier = builder.getLevel().getGameRules().
             getRule(VaultHuntersExtraRules.COPIOUSLY_DROP).get().getMultiplier();
 
-        // Do something only if there is a point to do something.
-        if (modifier > 1)
+        // Get starting list of all items.
+        List<ItemStack> originalList = new ArrayList<>(originalInstance);
+
+        for (int i = 0; i < modifier; i++)
         {
-            // get original drop
-            List<ItemStack> originalDrops = new ArrayList<>(drops.size() / 2);
-
-            for (int i = 0; i < drops.size() / 2; i++)
-            {
-                originalDrops.add(drops.get(i));
-            }
-
-            // Add all missing drops to the list.
-            for (int i = 1; i < modifier; i++)
-            {
-                drops.addAll(originalDrops);
-            }
+            // Create clone of original item list.
+            originalInstance.addAll(originalList.stream().map(ItemStack::copy).toList());
         }
+
+        return true;
     }
 }
