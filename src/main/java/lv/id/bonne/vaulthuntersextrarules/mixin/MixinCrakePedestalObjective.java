34902334bec9d12302087bbeb9ebb4edae17b6db
BONNe <bonne@bonne.id.lv>
2023-12-05 23:20:32 +0200

    Fixes Crake Pedestal still not allowing reusing.

--- src/main/java/lv/id/bonne/vaulthuntersextrarules/mixin/MixinCrakePedestalObjective.java
+++ src/main/java/lv/id/bonne/vaulthuntersextrarules/mixin/MixinCrakePedestalObjective.java
@@ -8,17 +8,15 @@ package lv.id.bonne.vaulthuntersextrarules.mixin;
 
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import iskallia.vault.block.CrakePedestalBlock;
 import iskallia.vault.core.event.common.BlockUseEvent;
 import iskallia.vault.core.vault.Vault;
 import iskallia.vault.core.vault.objective.CrakePedestalObjective;
 import lv.id.bonne.vaulthuntersextrarules.VaultHuntersExtraRules;
-import net.minecraft.world.level.Level;
 
 
 /**
@@ -29,54 +27,22 @@ import net.minecraft.world.level.Level;
 public class MixinCrakePedestalObjective
 {
     /**
-     * Sets the vault_hunters_extra_rules$world for variable.
-     * @param vault The vault.
-     * @param data The block use event data.
-     * @param ci Callback info.
+     * Replace block with a new crake pedestal. Other methods with assigning parameter value did not work.
+     * @param vault Vault.
+     * @param data Data.
+     * @param cir Callback info.
      */
     @Inject(method = "lambda$initServer$3",
-        at = @At(value = "HEAD"))
-    public void injectVariableAssign(Vault vault, BlockUseEvent.Data data, CallbackInfo ci)
-    {
-        vault_hunters_extra_rules$world = data.getWorld();
-    }
-
-
-    /**
-     * This method replaced false parameter with game rule value.
-     * @param par2 The parameter.
-     * @return The game rule value.
-     */
-    @ModifyArg(method = "lambda$initServer$3",
         at = @At(value = "INVOKE",
-            target = "net/minecraft/world/level/block/state/BlockState.setValue(Lnet/minecraft/world/level/block/state/properties/Property;Ljava/lang/Comparable;)Ljava/lang/Object;"),
-        index = 1)
-    private Comparable<?> addReusePedestal(Comparable<?> par2)
+            target = "Liskallia/vault/core/event/common/BlockUseEvent$Data;setResult(Lnet/minecraft/world/InteractionResult;)V",
+            ordinal = 1))
+    private void replaceCake(Vault vault,
+        BlockUseEvent.Data data,
+        CallbackInfo cir)
     {
-        return vault_hunters_extra_rules$world == null ||
-            !vault_hunters_extra_rules$world.getGameRules().
-                getRule(VaultHuntersExtraRules.REUSE_PEDESTALS).
-                get();
+        if (data.getWorld().getGameRules().getRule(VaultHuntersExtraRules.REUSE_PEDESTALS).get())
+        {
+            data.getWorld().setBlock(data.getPos(), data.getState().setValue(CrakePedestalBlock.CONSUMED, false), 3);
+        }
     }
-
-
-    /**
-     * Removes the vault_hunters_extra_rules$world value.
-     * @param vault The vault.
-     * @param data The block use event data.
-     * @param ci Callback info.
-     */
-    @Inject(method = "lambda$initServer$3",
-        at = @At(value = "RETURN"))
-    public void injectVariableRemove(Vault vault, BlockUseEvent.Data data, CallbackInfo ci)
-    {
-        vault_hunters_extra_rules$world = null;
-    }
-
-
-    /**
-     * The world variable.
-     */
-    @Unique
-    private static Level vault_hunters_extra_rules$world;
 }
