2aa0d8907f30a027fff795d55ad5c79d0c85f7de
BONNe <bonne@bonne.id.lv>
2024-02-02 18:09:39 +0200

    Rework copiously calculations to match VH update.

--- src/main/java/lv/id/bonne/vaulthuntersextrarules/mixin/MixinVaultBlockOre.java
+++ src/main/java/lv/id/bonne/vaulthuntersextrarules/mixin/MixinVaultBlockOre.java
@@ -7,21 +7,17 @@
 package lv.id.bonne.vaulthuntersextrarules.mixin;
 
 
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
 import iskallia.vault.block.VaultOreBlock;
 import lv.id.bonne.vaulthuntersextrarules.VaultHuntersExtraRules;
 import lv.id.bonne.vaulthuntersextrarules.util.GameRuleHelper;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 
 
 /**
@@ -31,37 +27,23 @@ import java.util.List;
 public class MixinVaultBlockOre
 {
     /**
-     * This method redirects to increaseLootQuantity.
-     * @param originalInstance The originalInstance.
-     * @param dropCopy The collection.
-     * @param state The block state.
+     * Redirects the amount of loot being dropped.
      * @param builder The loot context builder.
+     * @param cir Callback info.
      */
-    @Redirect(method = "getDrops",
-        at = @At(value = "INVOKE", target = "Ljava/util/List;addAll(Ljava/util/Collection;)Z"))
-    private boolean increaseCopiouslyDrop(List<ItemStack> originalInstance,
-        Collection<ItemStack> dropCopy,
-        BlockState state,
-        LootContext.Builder builder)
+    @Inject(method = "getCopiouslyChance", at = @At("TAIL"), cancellable = true)
+    private static void increaseCopiouslyDrop(LootContext.Builder builder, CallbackInfoReturnable<Float> cir)
     {
         Player player = null;
-        if (builder.getOptionalParameter(LootContextParams.THIS_ENTITY) instanceof Player entity) {
+
+        if (builder.getOptionalParameter(LootContextParams.THIS_ENTITY) instanceof Player entity)
+        {
             player = entity;
         }
 
         // Get modifier value
-        float modifier = GameRuleHelper.
-            getRule(VaultHuntersExtraRules.COPIOUSLY_DROP, builder.getLevel(), player).get().getMultiplier();
-
-        // Get starting list of all items.
-        List<ItemStack> originalList = new ArrayList<>(originalInstance);
-
-        for (int i = 0; i < modifier; i++)
-        {
-            // Create clone of original item list.
-            originalInstance.addAll(originalList.stream().map(ItemStack::copy).toList());
-        }
-
-        return true;
+        cir.setReturnValue(cir.getReturnValue() *
+            GameRuleHelper.getRule(VaultHuntersExtraRules.COPIOUSLY_DROP,
+                builder.getLevel(), player).get().getMultiplier());
     }
 }
