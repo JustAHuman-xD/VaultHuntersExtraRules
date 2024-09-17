9007f3ab26b95afe3cebcdcc4facb282de5aa024
BONNe <bonne@bonne.id.lv>
2024-09-17 13:28:26 +0300

    Simplify Spirit and Lodestone mixins using @Local.

--- src/main/java/lv/id/bonne/vaulthuntersextrarules/mixin/MixinLodestoneObjective.java
+++ src/main/java/lv/id/bonne/vaulthuntersextrarules/mixin/MixinLodestoneObjective.java
@@ -7,19 +7,16 @@
 package lv.id.bonne.vaulthuntersextrarules.mixin;
 
 
+import com.llamalad7.mixinextras.sugar.Local;
+
 import iskallia.vault.core.event.common.BlockUseEvent;
-import iskallia.vault.core.vault.Vault;
 import iskallia.vault.core.vault.objective.LodestoneObjective;
-import iskallia.vault.core.world.storage.VirtualWorld;
 import lv.id.bonne.vaulthuntersextrarules.VaultHuntersExtraRules;
 import lv.id.bonne.vaulthuntersextrarules.util.GameRuleHelper;
-import net.minecraft.world.entity.player.Player;
+
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 
 /**
@@ -30,21 +27,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class MixinLodestoneObjective
 {
     /**
-     * Sets the vault_hunters_extra_rules$world variable value.
-     * @param vault The vault.
-     * @param world The virtual world.
-     * @param data The block use event data.
-     * @param ci Callback info.
-     */
-    @Inject(method = "lambda$initServer$3",
-        at = @At(value = "HEAD"))
-    public void injectVariableAssign(Vault vault, VirtualWorld world, BlockUseEvent.Data data, CallbackInfo ci)
-    {
-        vault_hunters_extra_rules$player = data.getPlayer();
-    }
-
-
-    /**
      * This method replaced false parameter with game rule value.
      * @param consumed The parameter.
      * @return The game rule value.
@@ -53,32 +35,10 @@ public class MixinLodestoneObjective
         at = @At(value = "INVOKE",
             target = "iskallia/vault/block/entity/LodestoneTileEntity.setConsumed(Z)V"),
         index = 0)
-    private boolean addReuseLodestone(boolean consumed)
+    private boolean addReuseLodestone(boolean consumed, @Local(ordinal = 0, argsOnly = true) BlockUseEvent.Data data)
     {
-        return vault_hunters_extra_rules$player == null ||
-            !GameRuleHelper.
-                getRule(VaultHuntersExtraRules.REUSE_PEDESTALS, vault_hunters_extra_rules$player).
-                get();
+        return !GameRuleHelper.
+            getRule(VaultHuntersExtraRules.REUSE_PEDESTALS, data.getPlayer()).
+            get();
     }
-
-
-    /**
-     * Removes the vault_hunters_extra_rules$world value.
-     * @param vault The vault.
-     * @param world The virtual world.
-     * @param data The block use event data.
-     * @param ci Callback info.
-     */
-    @Inject(method = "lambda$initServer$3",
-        at = @At(value = "RETURN"))
-    public void injectVariableRemove(Vault vault, VirtualWorld world, BlockUseEvent.Data data, CallbackInfo ci)
-    {
-        vault_hunters_extra_rules$player = null;
-    }
-
-    /**
-     * The player variable.
-     */
-    @Unique
-    private static Player vault_hunters_extra_rules$player;
 }
--- src/main/java/lv/id/bonne/vaulthuntersextrarules/mixin/MixinSpiritEntity.java
+++ src/main/java/lv/id/bonne/vaulthuntersextrarules/mixin/MixinSpiritEntity.java
@@ -7,20 +7,16 @@
 package lv.id.bonne.vaulthuntersextrarules.mixin;
 
 
+import com.llamalad7.mixinextras.sugar.Local;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import iskallia.vault.entity.entity.SpiritEntity;
 import iskallia.vault.init.ModGameRules;
 import iskallia.vault.world.VaultMode;
 import lv.id.bonne.vaulthuntersextrarules.util.GameRuleHelper;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.player.Player;
-import net.minecraftforge.event.entity.living.LivingDeathEvent;
 
 
 /**
@@ -30,21 +26,6 @@ import net.minecraftforge.event.entity.living.LivingDeathEvent;
 public class MixinSpiritEntity
 {
     /**
-     * Sets the vault_hunters_extra_rules$player variable value.
-     * @param event The event for player death.
-     * @param ci Callback info.
-     */
-    @Inject(method = "onPlayerDeath", at = @At(value = "HEAD"), remap = false)
-    private static void injectVariableAssign(LivingDeathEvent event, CallbackInfo ci)
-    {
-        if (event.getEntity() instanceof ServerPlayer player)
-        {
-            vault_hunters_extra_rules$player = player;
-        }
-    }
-
-
-    /**
      * This method changes gamerule return value based on player vault mode.
      * @param instance The original gamerule instance.
      * @return The adjusted gamemode value.
@@ -52,26 +33,9 @@ public class MixinSpiritEntity
     @Redirect(method = "lambda$onPlayerDeath$4",
         at = @At(value = "INVOKE", target = "Liskallia/vault/world/VaultMode$GameRuleValue;get()Liskallia/vault/world/VaultMode;"),
         remap = false)
-    private static VaultMode onPlayerDeath(VaultMode.GameRuleValue instance)
+    private static VaultMode onPlayerDeath(VaultMode.GameRuleValue instance,
+        @Local(ordinal = 0, argsOnly = true) ServerPlayer player)
     {
-        return GameRuleHelper.getRule(ModGameRules.MODE, vault_hunters_extra_rules$player).get();
+        return GameRuleHelper.getRule(ModGameRules.MODE, player).get();
     }
-
-
-    /**
-     * Removes the vault_hunters_extra_rules$player value.
-     * @param event The event for player death.
-     * @param ci Callback info.
-     */
-    @Inject(method = "onPlayerDeath", at = @At(value = "RETURN"), remap = false)
-    private static void injectVariableRemove(LivingDeathEvent event, CallbackInfo ci)
-    {
-        vault_hunters_extra_rules$player = null;
-    }
-
-    /**
-     * The player variable.
-     */
-    @Unique
-    private static Player vault_hunters_extra_rules$player;
 }
